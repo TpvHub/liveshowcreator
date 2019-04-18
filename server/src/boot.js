af46968fe0257e6cf3bef277e1c6c7232d10dace
tpv <tinh.phan.v@gmail.com>
2019-04-18 08:41:03 +0700

    add new roles

--- server/src/boot.js
+++ server/src/boot.js
@@ -1,44 +1,58 @@
 import { rootUser } from './config'
+import { composePromise } from './utils/common'
 import _ from 'lodash'
+
+const checkExistRole = ({ role: roleName, ctx }) => new Promise(async (rs, rj) => {
+  ctx.models.role.findOne({
+    name: roleName,
+  }).then(role => {
+    rs({ ctx, isRoleExist: role !== null, roleName })
+  }).catch(rj)
+})
+
+const createNewRole = ({ ctx, isRoleExist, roleName }) => new Promise(async (rs, rj) => {
+  if (isRoleExist) rs(true)
+  else {
+    try {
+      await ctx.models.role.save(null, {
+        name: roleName,
+      })
+      rs(ctx)
+    } catch(err) {
+      rj(err)
+    }
+  }
+})
 
 export default async (ctx) => {
 
-  let administratorRole = await ctx.models.role.findOne(
-    {name: 'administrator'})
-  let defaultUser = await ctx.models.user.findOne({email: rootUser.email})
+  try {
+    await Promise.all(
+      ['administrator', 'staff', 'client', 'user'].map(role => composePromise(
+        createNewRole,
+        checkExistRole
+      )({ ctx, role }))
+    )
 
-  if (defaultUser) {
-    // let update roles administrator
-    let roles = _.get(defaultUser, 'roles', [])
-    if (roles === null) {
-      roles = []
+    let defaultUser = await ctx.models.user.findOne({ email: rootUser.email })
+
+    if (defaultUser) {
+      // let update roles administrator
+      let roles = _.get(defaultUser, 'roles', [])
+      if (roles === null) {
+        roles = []
+      }
+      roles.push('administrator')
+      roles = _.uniq(roles)
+      // let update user
+      await ctx.models.user.updateUserRoles(defaultUser._id, roles)
+    } else {
+      // create default user;
+      await ctx.models.user.insertOne(rootUser)
     }
-    roles.push('administrator')
-    roles = _.uniq(roles)
-    // let update user
-    await ctx.models.user.updateUserRoles(defaultUser._id, roles)
-  }
-  if (administratorRole === null) {
-    // create role
-    administratorRole = await ctx.models.role.save(null,
-      {name: 'administrator'})
-  }
 
-  // check and create new role is staff
-  let staffRole = await ctx.models.role.findOne({
-    name: 'staff',
-  })
-
-  if (staffRole === null) {
-    await ctx.models.role.save(null, {
-      name: 'staff',
-    })
-  }
-
-  if (administratorRole && defaultUser === null) {
-    // create default user;
-    await ctx.models.user.insertOne(rootUser)
-
+  } catch (err) {
+    console.log('Boot ERROR: ', err.message, err)
   }
 
 }
