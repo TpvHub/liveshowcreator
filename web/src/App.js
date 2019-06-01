4b41a5b3247d2375d1d8ff0d72867b3ae0d9b608
tpv <tinh.phan.v@gmail.com>
2019-06-01 16:24:15 +0700

    add custom theme

--- web/src/App.js
+++ web/src/App.js
@@ -6,26 +6,36 @@ import ErrorMessage from './components/error-message'
 import Message from './components/message'
 import LoadingDialog from './components/dialog/loading-dialog'
 
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#026ca0' }, // Purple and blueGrey play nicely together.
+    secondary: { main: '#7b1fa2' }
+  },
+});
+
 class App extends Component {
-  render () {
+  render() {
     return (
-      <Fragment>
+      <MuiThemeProvider theme={theme}>
         <Router history={history}>
           <Switch>
             {
               routes.map((route, index) => {
                 return (
-                  <Route key={`Route-${route.path}-${index}`} exact path={route.path} component={route.component}/>
+                  <Route key={`Route-${route.path}-${index}`} exact path={route.path} component={route.component} />
                 )
               })
             }
 
           </Switch>
         </Router>
-        <ErrorMessage/>
-        <Message/>
+        <ErrorMessage />
+        <Message />
         <LoadingDialog />
-      </Fragment>
+      </MuiThemeProvider>
     )
   }
 }
